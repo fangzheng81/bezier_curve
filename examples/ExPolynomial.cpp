f51951747e5acc241d8c1b78735d7fb1de0297e1
xmba15 <thba1590@gmail.com>
2019-08-12 12:40:09 +0900

    add polynomial operations

--- examples/ExPolynomial.cpp
+++ examples/ExPolynomial.cpp
@@ -16,11 +16,22 @@
 
 int main(int argc, char* argv[])
 {
-    std::vector<double> coeffs{10.5, 4.7, 9.2, 0};
+    std::vector<double> coeffs{1, -1, 5, -2};
+    std::vector<double> otherCoeffs{-2, 3};
 
     robotics::maths::PowerBasisPolynomial<double> polynomial(coeffs);
+    robotics::maths::PowerBasisPolynomial<double> otherPolynomial(otherCoeffs);
 
+    auto sum = polynomial.add(otherPolynomial);
+    auto multiplication = polynomial.multiply(otherPolynomial);
 
-    std::cout << polynomial << "\n";
+    std::cout << multiplication << "\n";
+    std::cout << multiplication.derivative() << "\n";
+
+    robotics::maths::PowerBasisPolynomial<double> polynomial0(
+        std::vector<double>{8.5, 3.45, 0.0, 0.0, 5.7, 4.9, 0.1, 10});
+
+    std::cout << polynomial0 << "\n";
+
     return 0;
 }
--- include/bezier/PowerBasisPolynomial.hpp
+++ include/bezier/PowerBasisPolynomial.hpp
@@ -33,12 +33,22 @@ template <typename DATA_TYPE = double> class PowerBasisPolynomial
 
     const size_t degree() const;
 
+    PowerBasisPolynomial add(const PowerBasisPolynomial& other) const;
+
+    PowerBasisPolynomial multiply(const PowerBasisPolynomial& other) const;
+
+    PowerBasisPolynomial derivative() const;
+
     friend std::ostream& operator<<(std::ostream& os, const PowerBasisPolynomial& polynomial)
     {
         assert(polynomial.degree() >= 0);
 
         os << "Polynomial function: \n";
         for (size_t i = 0; i < polynomial.coeffs().size(); ++i) {
+            if (fuzzyEquals(polynomial.coeffs()[i], 0.0)) {
+                continue;
+            }
+
 #if ENABLE_DEBUG
             os << " + " << std::setw(8) << polynomial.coeffs()[i] << " * std::pow(t," << i << ")";
 #else
@@ -79,6 +89,65 @@ template <typename DATA_TYPE> const std::deque<DATA_TYPE>& PowerBasisPolynomial<
 template <typename DATA_TYPE> const size_t PowerBasisPolynomial<DATA_TYPE>::degree() const
 {
     return this->_coeffs.size() - 1;
+}
+
+template <typename DATA_TYPE>
+PowerBasisPolynomial<DATA_TYPE> PowerBasisPolynomial<DATA_TYPE>::add(const PowerBasisPolynomial<DATA_TYPE>& other) const
+{
+    std::deque<DATA_TYPE> biggerDegCoeffs =
+        this->_coeffs.size() >= other._coeffs.size() ? this->_coeffs : other._coeffs;
+    std::deque<DATA_TYPE> smallerDegCoeffs =
+        this->_coeffs.size() < other._coeffs.size() ? this->_coeffs : other._coeffs;
+
+    for (size_t i = 0; i < smallerDegCoeffs.size(); ++i) {
+        biggerDegCoeffs[i] += smallerDegCoeffs[i];
+    }
+
+    return PowerBasisPolynomial<DATA_TYPE>(biggerDegCoeffs);
+}
+
+template <typename DATA_TYPE = double>
+PowerBasisPolynomial<DATA_TYPE> addPolynomials(const PowerBasisPolynomial<DATA_TYPE>& p1,
+                                               const PowerBasisPolynomial<DATA_TYPE>& p2)
+{
+    return p1.add(p2);
+}
+
+template <typename DATA_TYPE>
+PowerBasisPolynomial<DATA_TYPE>
+PowerBasisPolynomial<DATA_TYPE>::multiply(const PowerBasisPolynomial<DATA_TYPE>& other) const
+{
+    std::deque<DATA_TYPE> resultCoeffs(this->_coeffs.size() + other._coeffs.size(), 0.0);
+
+    for (size_t i = 0; i < this->_coeffs.size(); ++i) {
+        for (size_t io = 0; io < other._coeffs.size(); ++io) {
+            resultCoeffs[i + io] += this->_coeffs[i] * other._coeffs[io];
+        }
+    }
+
+    return PowerBasisPolynomial<DATA_TYPE>(resultCoeffs);
+}
+
+template <typename DATA_TYPE = double>
+PowerBasisPolynomial<DATA_TYPE> multiplyPolynomials(const PowerBasisPolynomial<DATA_TYPE>& p1,
+                                                    const PowerBasisPolynomial<DATA_TYPE>& p2)
+{
+    return p1.multiply(p2);
+}
+
+template <typename DATA_TYPE> PowerBasisPolynomial<DATA_TYPE> PowerBasisPolynomial<DATA_TYPE>::derivative() const
+{
+    assert(this->_coeffs.size() > 0);
+    if (this->_coeffs.size() == 1) {
+        return PowerBasisPolynomial(std::deque<DATA_TYPE>{0});
+    }
+
+    std::deque<DATA_TYPE> derivCoeffs(this->_coeffs.begin() + 1, this->_coeffs.end());
+    for (size_t i = 0; i < derivCoeffs.size(); ++i) {
+        derivCoeffs[i] *= (i + 1);
+    }
+
+    return PowerBasisPolynomial(derivCoeffs);
 }
 
 template <typename DATA_TYPE> void PowerBasisPolynomial<DATA_TYPE>::removeLeadingZeros()
